698d6bc0b961702ecf12ab76f3e296853829fd92
Wataru Oshima <wataru.bb.tokyo@gmail.com>
2024-01-12 17:14:27 +0900

    added installopencv.sh:

---
+++ installOpenCV.sh
@@ -0,0 +1,21 @@
+cd $HOME
+git clone -b v4.1.1 https://github.com/opencv/opencv.git
+cd opencv
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Define the build directory
+BUILD_DIR="build"
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
+
+# Configure the project
+# Replace <Your_Project_Source_Dir> with the actual source directory of your project
+# Specify the CMake options you need
+cmake .. -DWITH_CUDA=ON -DWITH_GSTREAMER=ON
+
+# Build the project
+make
+
+# Install the project
+sudo make install
