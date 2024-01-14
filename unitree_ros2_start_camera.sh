0afeeae26d88dde0ab810749ec165cadf45b3f7c
Wataru Oshima <wataru.bb.tokyo@gmail.com>
2024-01-14 17:03:01 +0900

    updated start_camera.sh

--- unitree_ros2_start_camera.sh
+++ unitree_ros2_start_camera.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo "Start publishing the camera image"
+exec > /tmp/unitree_ros2_start_camera.log 2>&1
 source /opt/ros/foxy/setup.bash
-source $HOME/unitree_ws/install/setup.bash
-
+source /home/unitree/unitree_ws/install/setup.bash
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 # Loop until the specified address is reachable
 while ! ping -c 1 192.168.123.161 &> /dev/null; do
     echo "Waiting for 192.168.123.161 to become reachable..."
